a0f0b022c756702cb444ed6f6ae119e17f4ab31b
Badr Adnani <zouhairadnani46@gmail.com>
2024-04-18 20:26:08 +0100

    Initial commit

---
+++ javascript/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Lo-fi Beat - BENT NAS",
+        artist: "TOTO",
+        img: "adventuretime",
+        src: "BENTNAS"
+    },
+    {
+        name: "Lo-fi Beat - Dawk lia",
+        artist: "SNOR",
+        img: "arcane",
+        src: "DAWKLIA"
+    },
+    {
+        name: "Lo-fi Beat - HYATI",
+        artist: "DUKE",
+        img: "arcane",
+        src: "DUKE"
+    },
+    {
+        name: "Lo-fi Beat - DELLALI",
+        artist: "TOTO",
+        img: "blindinlove",
+        src: "DELLALI"
+    },
+    {
+        name: "Lo-fi Beat - HARAM",
+        artist: "TOTO",
+        img: "bluemoon",
+        src: "HARAM"
+    },
+    {
+        name: "Lo-fi Beat - TRAP",
+        artist: "KOUZ1",
+        img: "closetoyou",
+        src: "TRAP"
+    },
+]
